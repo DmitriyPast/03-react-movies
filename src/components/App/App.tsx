4d1f844bc819934315eca15ba00e94092824c083
your_name <pastushenkodmitry2003@gmail.com>
2025-08-01 18:15:09 +0300

    add css modules

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import "./App.css";
+import css from "./App.module.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className={css.app}></div>
+    </>
+  );
+}
+
+export default App;
